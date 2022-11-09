257b18618267442b67767f5b12ede128cb47b82d
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-09 17:27:37 +0700

    move server.js outside the src folder

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const studentRoutes = require("./src/student/routes");
+
+const app = express();
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.json({ message: "Hello World" });
+});
+
+app.use("/api/v1/students", studentRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
