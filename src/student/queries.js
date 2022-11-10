3408503ef13d994876b7a4e31ec27a41ba369e4c
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-10 15:51:10 +0700

    create query for update student by id

--- src/student/queries.js
+++ src/student/queries.js
@@ -4,6 +4,10 @@ const addNewStudent =
   "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
 const isEmailExists = "SELECT * FROM students WHERE email = $1";
 const isNameExists = "SELECT * FROM students WHERE name = $1";
+const updateStudent =
+  "UPDATE students SET name = $1, email = $2, age = $3, dob = $4 WHERE id = $5";
+
+const deleteStudent = "DELETE FROM students WHERE id = $1";
 
 module.exports = {
   getAllStudents,
@@ -11,4 +15,6 @@ module.exports = {
   addNewStudent,
   isEmailExists,
   isNameExists,
+  updateStudent,
+  deleteStudent,
 };
