dedfc55656d81d151080f92a7023827e105e58ae
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-10 15:36:52 +0700

    add new query for checking the existence of name and email

--- src/student/queries.js
+++ src/student/queries.js
@@ -1,7 +1,14 @@
 const getAllStudents = "SELECT * FROM students";
 const getStudentById = "SELECT * FROM students WHERE id = $1";
+const addNewStudent =
+  "INSERT INTO students (name, email, age, dob) VALUES ($1, $2, $3, $4)";
+const isEmailExists = "SELECT * FROM students WHERE email = $1";
+const isNameExists = "SELECT * FROM students WHERE name = $1";
 
 module.exports = {
   getAllStudents,
   getStudentById,
+  addNewStudent,
+  isEmailExists,
+  isNameExists,
 };
