a50eebd43ed7bec375dc2bb39fa1d69e69cce6e6
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-10 15:37:36 +0700

    add new controller for handling post (add new student)

--- src/student/controller.js
+++ src/student/controller.js
@@ -21,7 +21,43 @@ const getStudentById = (req, res) => {
   });
 };
 
+const addNewStudent = (req, res) => {
+  const { name, email, age, dob } = req.body; // destructuring the request body
+
+  pool.query(queries.isEmailExists, [email], (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    if (results.rows.length > 0) {
+      res.status(409).json({ message: "Email already exists" });
+    } else {
+      pool.query(queries.isNameExists, [name], (err, results) => {
+        if (err) {
+          throw err;
+        }
+
+        if (results.rows.length > 0) {
+          res.status(409).json({ message: "Name already exists" });
+        } else {
+          pool.query(
+            queries.addNewStudent,
+            [name, email, age, dob],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              res.status(201).json({ message: "Student added successfully" });
+            }
+          );
+        }
+      });
+    }
+  });
+};
+
 module.exports = {
   getStudents,
   getStudentById,
+  addNewStudent,
 };
