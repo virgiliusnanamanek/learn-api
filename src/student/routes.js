28abfd89eb40057da9ddae23035f0a2b8afba1c9
virgiliusnanamanek <vickynana034@gmail.com>
2022-11-10 16:15:49 +0700

    create new route for delete student

--- src/student/routes.js
+++ src/student/routes.js
@@ -7,5 +7,6 @@ router.get("/", controller.getStudents);
 router.get("/:id", controller.getStudentById);
 router.post("/", controller.addNewStudent);
 router.put("/:id", controller.updateStudent);
+router.delete("/:id", controller.deleteStudent);
 
 module.exports = router;
